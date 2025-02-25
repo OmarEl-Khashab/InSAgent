8d8bd4953bc18818df860ed32da63601dcd1d149
omarelkhashab <omar_khashab11@hotmail.com>
2025-02-25 23:29:44 +0200

    adjusted the rep structure

--- tools.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.tools import tool
-
-tavily_tool = TavilySearchResults(max_results=5)
